cfb043b3221171410fdb38d6f31873562de6002f
demunter jelter <demunterjelter@hotmail.com>
2018-09-24 11:38:53 +0200

    initial commit maven project jelter

--- src/main/java/com/realdolmen/gitmaven/GitClass.java
+++ src/main/java/com/realdolmen/gitmaven/GitClass.java
@@ -5,4 +5,8 @@ public class GitClass {
     public void test(){
         System.out.println("Hello Git");
     }
+    public void test2(){
+        System.out.println("hello gsemeese");
+    }
+    
 }
